4118f07ad3a886262fbcdfa378dfe57292018455
bryan.zera <bryan.zera@ac5bcbaf-1c45-0410-b09b-a165f8c081d9>
2008-02-01 19:40:46 +0000

    removed old login info

--- trunk/config.php
+++ trunk/config.php
@@ -1,11 +1,11 @@
 <?php
 
-	$http_path = 'http://web2.colum.edu/shortur/';
-	$db_host = 'cms.colum.edu';
-	$db_username = 'shortur';
-	$db_password = 'shortur';
-	$db_name = 'shortur';
-	$domain = 'http://www.colum.edu/';
-	$cookie_name = 'shortur_auth';	
+	$http_path = '';
+	$db_host = '';
+	$db_username = '';
+	$db_password = '';
+	$db_name = '';
+	$domain = '';
+	$cookie_name = '';	
 	
 ?>
