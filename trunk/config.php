0572c95c5c22355054fff9e28892d014f1af3142
matthew.mcclintock <matthew.mcclintock@ac5bcbaf-1c45-0410-b09b-a165f8c081d9>
2008-02-20 21:18:43 +0000

    removed extraneous login stuff

--- trunk/config.php
+++ trunk/config.php
@@ -1,13 +1,12 @@
 <?php
 
-	$http_path = 'http://web2.colum.edu/shortur_dev/shortur/';
-	$db_host = 'cms.colum.edu';
-	$db_username = 'shortur';
-	$db_password = 'shortur';
-	$db_name = 'shortur_dev';
-	$base_url = 'http://www.colum.edu/';
-	$external_404_page = '';
-	$cookie_name = 'shortur_auth';
+	$http_path = '';
+	$db_host = '';
+	$db_username = '';
+	$db_password = '';
+	$db_name = '';
+	$domain = '';
+	$cookie_name = 'shortur_auth'; 
 	$per_page = 15;
 	$show_pages = 2;
 	
