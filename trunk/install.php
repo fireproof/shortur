fd949c08de6e7ab669a2736308f3dd5ddc0234eb
bryan.zera <bryan.zera@ac5bcbaf-1c45-0410-b09b-a165f8c081d9>
2008-05-05 19:11:14 +0000

    added forgotten password functionality

--- trunk/install.php
+++ trunk/install.php
@@ -14,9 +14,11 @@
 	$db_password = $_REQUEST['db_password'];
 	$db_name = d($_REQUEST['db_name'], 'shortur');
 	$db_exists = $_REQUEST['db_exists'];
+	$admin_email = $_REQUEST['admin_email'];
 	$admin_password = $_REQUEST['admin_password'];
 	$base_url = d($_REQUEST['base_url'], 'http://' . $_SERVER['HTTP_HOST'] . '/');
 	$external_404_page = $_REQUEST['external_404_page'];
+	$email_address = $_REQUEST['email_address'];
 	
 	// can this user write to the config file?
 	if (!is_writeable($config_file)) {
@@ -49,12 +51,18 @@
 			
 		if (!$db_password)
 			$data['errors'][] = "You must specify a database password.";
-			
+	
+		if (!$admin_email)
+			$data['errors'][] = "You must enter an email address for the admin user.";
+	
 		if (!$admin_password)
 			$data['errors'][] = "You must specify an admin password.";
 
 		if (!$base_url)
 			$data['errors'][] = "You must specify a base URL.";
+			
+		if (!$email_address)
+			$data['errors'][] = "You must specify an email address.";
 
 		if ($db_host && $db_username && $db_password && $db_name && $admin_password) {
 			
@@ -79,7 +87,7 @@
 		
 		if (!$data['errors']) {
 
-			write_settings($http_path, $db_host, $db_username, $db_password, $db_name, $base_url);
+			write_settings($http_path, $db_host, $db_username, $db_password, $db_name, $base_url, $email_address);
 			
 			mysql_select_db($db_name);	
 			
@@ -94,12 +102,13 @@
 			$create_table_users_sql = "CREATE TABLE `users` ( " .
 				"`id` int(11) NOT NULL auto_increment," .
 				"`username` text NOT NULL," .
+				"`email` text NOT NULL," .
 				"`password` text NOT NULL," .
 				"`admin` tinyint(1) NOT NULL," .
 				"PRIMARY KEY  (`id`))";
 
-			$create_admin_user_sql = "insert into `users` (username, password, admin) values " . 
-				"('admin', '" . md5($admin_password) . "', 1)";
+			$create_admin_user_sql = "insert into `users` (username, email, password, admin) values " . 
+				"('admin', '" mysql_real_escape_string($admin_email) . "', '" . md5($admin_password) . "', 1)";
 
 			q($create_table_entries_sql);
 			q($create_table_users_sql);
@@ -164,19 +173,31 @@ EOF;
 				<input type='text' name='base_url' value='$base_url' />
 				<span class='explaination'>The base URL for target URLs.</span>
 			</div>
+
+			<div class='line_item'>
+				<b>Admin Password:</b>
+				<input type='password' name='admin_email' />
+				<span class='explaination'>The email address for the initial admin user.</span>
+			</div>
 			
 			<div class='line_item'>
 				<b>Admin Password:</b>
 				<input type='password' name='admin_password' />
-				<span class='explaination'>The password for the first username, 'admin'.</span>
+				<span class='explaination'>The password for the initial admin user.</span>
 			</div>
 			
 			<div class='line_item_alt'>
 				<b>External 404 Page:</b>
 				<input type='text' name='external_404_page' value='$external_404_page' />
 				<span class='explaination'>If left blank, ShortUr will generate a generic "Page Not Found" error.</span>
-				
 			</div>
+
+			<div class='line_item'>
+				<b>Email Address:</b>
+				<input type='text' name='email_address' value='$email_address' />
+				<span class='explaination'>Password reset emails will come from this email address.</span>
+			</div>			
+			
 			<div class='line_item_alt' style='text-align: center;'>
 				<input type='submit' name='submit' value='Install ShortUr'>
 			</div>
@@ -190,7 +211,7 @@ EOF;
 	template($data, false);
 
 	function write_settings($in_http_path=null, $in_db_host=null, $in_db_username=null, 
-		$in_db_password=null, $in_db_name=null, $in_base_url=null, $in_external_404_page=null) {
+		$in_db_password=null, $in_db_name=null, $in_base_url=null, $in_external_404_page=null, $email=null) {
 
 		global $config_file, $http_path, $db_host, $db_username, $db_password, $base_url, $cookie_name;
 
@@ -207,6 +228,7 @@ EOF;
 	\$cookie_name = 'shortur_auth';
 	\$per_page = 20;
 	\$show_pages = 2;
+	\$from_email = '$email';
 	
 ?>
 EOF;
